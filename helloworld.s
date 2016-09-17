e87a4553d8e82c15ad4c6f26999128e6221d215d
Smashtina <christina_luo96@hotmail.com>
2016-09-17 15:54:48 +0200

    Create helloworld.s

---
+++ helloworld.s
@@ -0,0 +1,28 @@
+#include <iregdef.h>
+.data
+word: 
+.asciiz "Hello world\n"
+word2: 
+.asciiz "Christina is mass effect master everyday\n"
+
+.text
+.set noreorder
+.global start
+.ent start
+start:
+	subu sp,sp,24 //
+	sw ra,20(sp) //
+	
+	la a0,word //lägger till adressen till word i a0
+	jal printf
+	nop			//fyller med nollor
+	
+	la a0,word2 
+	jal printf
+	nop	
+	
+	lw ra,20(sp)
+	addiu sp,sp,24
+	jr ra
+.end start
+
