ec978601232d4b3645fa7865bbdc6c3554979196
Smashtina <christina_luo96@hotmail.com>
2016-09-19 16:17:25 +0200

    Update Labzor2.s

--- Labzor2.s
+++ Labzor2.s
@@ -3,6 +3,8 @@
 
 .data
 newLine: .asciiz "new line\n"
+array: .word 4,5,2,2,1,6,7,9,5,10
+//antal: word 10
 
 
 .text
@@ -11,18 +13,25 @@ newLine: .asciiz "new line\n"
 .ent skriv
 
 skriv:
-  subu sp,sp,24			//flyttar stackpekaren
-  sw ra,20(sp)       //sparar returadresssen
+subu sp,sp,24			//flyttar stackpekaren
+sw ra,20(sp)       //sparar returadresssen
 
-  la a0, newLine  //lägger till adressen till newLine i a0
-  jal printf		
-  nop
+li t0,20
+L1:
+ble t0,10,L2
+la a0, newLine  //lägger till adressen till newLine i a0
+jal printf		
+nop
+addi t0,t0,-1;
+b L1
+nop
 
-  lw ra,20(sp) // laddar in returadressen
-  addiu sp,sp,24 //flyttar tillbaka stackpekaren
-  jr ra
-  nop
-  .end skriv
+L2:
+lw ra,20(sp) // laddar in returadressen
+addiu sp,sp,24 //flyttar tillbaka stackpekaren
+jr ra
+nop
+.end skriv
 
 
 .globl start
@@ -30,14 +39,14 @@ skriv:
 
 start:
 
-  subu sp,sp,24
-  sw ra,20(sp)
+subu sp,sp,24
+sw ra,20(sp)
 
-  jal skriv		//kör subrutinen skriv
-  nop
+jal skriv		//kör subrutinen skriv
+nop
 
-  lw ra,20(sp) //laddar in returadressen
-  addiu sp,sp,24 //flyttar stackpekaren
-  jr ra
-  nop
-  .end start
+lw ra,20(sp) //laddar in returadressen
+addiu sp,sp,24 //flyttar stackpekaren
+jr ra
+nop
+.end start
