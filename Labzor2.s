dab5ecacabd559d3fdc1d2761b903954d2aeef74
Smashtina <christina_luo96@hotmail.com>
2016-09-22 16:00:32 +0200

    Update Labzor2.s

--- Labzor2.s
+++ Labzor2.s
@@ -37,6 +37,7 @@ skriv:
 
 	L2:
 	lw ra,20(sp) // laddar in returadressen
+	nop
 	addiu sp,sp,24 //flyttar tillbaka stackpekaren
 	jr ra
 	nop
@@ -54,6 +55,11 @@ quicksort:
 	jal partition
 	nop
 	
+	jal quicksort
+	nop
+	
+	jal quicksort
+	
 	
 	b LS1
 	nop
@@ -62,6 +68,7 @@ quicksort:
 	
 	LS2:
 	lw ra, 20(sp)
+	nop
 	addiu sp,sp,24
 	.end quicksort
 
@@ -81,37 +88,66 @@ partition:
 	
 	la t1,array //pivot ska bli t1, behöver första adressen senare
 
-	//s0*4	
-//	li t2,0 
-//	sll t2,t2,2
-//	addu t1,t2
+	
 	
 	li t6,4
 	mult s0,t6 // multiplicerar [a] med 4 för att få rätt plats i arrayen
 	mflo t6 
-	addu t1,t1,t6 //
+	addu t1,t1,t6 // få adress till 
 	lw t3,0(t1) //v[a] hamnar i t3 (pivot)
+	nop
 	lw s1,4(t1) //s1 = v[lower]
-	addu t1,4 //lower
+	nop
+	addu t9, s0,1   //lower=t9
+
 	
+	
+	li t7,4
+	mult s2,t7
+	mflo t7
+	addu t7,t0,t7
+	lw t7,0(t7) 		//fixar v[upper]
 	
 	LP1:
 	bgt s1,t3, LP2  // om v[lower]>pivot, hoppa till LP2
-	blt t1,s2, LP2 // om lower < upper, hoppa till LP2
-	
-	
+	bgt t9,s2, LP2 // om lower > upper, hoppa till LP2
+	addi t9,t9,1
 	b LP1
 	nop
 	
 	
 	LP2:
+	ble t7,t3,LP3			//om v[upper] =< pivot hoppa till LP3
+	bgt t9,s2,LP3			//om lower > upper hoppa till LP3
+	addi t9,t9,1
 	
+	b LP2
+	nop
 	
-	
-	b LP3
 
-	LP3:	
+
+	LP3:
+	bgt t9,s2,LP4
+	move t8,s1      //t8 är "temp"
+	move s1,t7
+	move t7,t8
+	addi t9,t9,1
+	addi s2,s2,-1
+	b LP3
+	
+	LP4:
+	ble t9,s2,LP1
+	b LPX
+	nop
+	
+	LPX:
+	move t8,t7
+	move t7,t3
+	move t3,t8
+	
+	//return s2 (k)	
 	lw ra, 20(sp)
+	nop
 	addiu sp,sp,24
 	.end partition
 
@@ -124,20 +160,24 @@ start:
 
 	subu sp,sp,24
 	sw ra,20(sp)
-	lw s2,antal
-	subu s2, 1
-	li s0,0
+	lw s2,antal //upper (kallas b här)
+	nop
+	addiu s2,s2,-1
+	li s0,0  // a
 	
 	
 	jal skriv		//kör subrutinen skriv
 	nop
 	jal quicksort
 	nop
+	jal skriv
+	nop
 	
 	
 	
 	
 	lw ra,20(sp) //laddar in returadressen
+	nop
 	addiu sp,sp,24 //flyttar stackpekaren
 	jr ra
 	nop
